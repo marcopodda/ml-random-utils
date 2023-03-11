b91f4f99e6062596d53595606ac69eb4ba26ca4f
Marco Podda <marco.x.podda@gsk.com>
2023-03-11 12:16:14 +0100

    moved parallelize to decorators.py

--- ml_utils/decorators.py
+++ ml_utils/decorators.py
@@ -1,8 +1,40 @@
 """General-purpose decorators."""
 
 import time
-from typing import Callable
+from typing import Callable, Iterable
 from functools import wraps
+from multiprocessing import cpu_count
+
+from joblib import Parallel, delayed
+
+
+def parallelize(**kwargs) -> Callable:
+    """
+    Returns a function that takes an iterable and calls it with
+    each item of the iterable in parallel using joblib. If `item`
+    is not a single element (e.g. a tuple or a list), it is the
+    responsibility of the function to which the decorator is
+    applied to unpack it. If not provided, kwarg argument `n_jobs`
+    will be set to the number of available CPUs minus 2.
+
+    Arguments:
+        **kwargs: arguments to pass to joblib.Parallel
+    Returns:
+        a function that can be called with an iterable as argument.
+        Each function call will be processed in parallel using joblib.
+    """
+    kwargs["n_jobs"] = kwargs.get("n_jobs", max(cpu_count() - 2, 1))
+
+    def wrapper(func: Callable) -> Callable:
+
+        @wraps(func)
+        def inner(items: Iterable) -> Iterable | None:
+            executor = Parallel(**kwargs)
+            job = delayed(func)
+            return executor(job(item) for item in items)
+        return inner
+
+    return wrapper
 
 
 def timeit(func: Callable) -> Callable:
